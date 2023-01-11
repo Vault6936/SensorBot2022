4b237a942d1a240b621e669ceaa13e3335a4079a
2amateurs <samuelegeelhood@gmail.com>
2023-01-11 11:59:15 -0500

    removed unnecessary code

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -9,26 +9,19 @@ import frc.robot.utils.SwitchProLayout;
 public class IO {
     private static IO instance = null;
     public int driveControllerPort;
-    public int ballControllerPort;
 
     public Joystick driveController;
-    public Joystick ballController;
 
     GamepadLayout driveLayout = new SwitchProLayout(); //drive controller layout
-    GamepadLayout ballLayout = new SwitchProLayout(); //tennis ball controller layout
 
     AxisMapping driveYVel;
     AxisMapping driveXVel;
-    AxisMapping driveRot;
 
     private IO() {
         this.driveControllerPort = Constants.DRIVE_CONTROLLER;
-        this.ballControllerPort = Constants.PAYLOAD_CONTROLLER;
         this.driveController = new Joystick(driveControllerPort);
-        this.ballController = new Joystick(ballControllerPort);
         this.driveYVel = new AxisMapping.Builder(this.driveController, driveLayout.driveYVel).build();
         this.driveXVel = new AxisMapping.Builder(this.driveController, driveLayout.driveXVel).build();
-        this.driveRot = new AxisMapping.Builder(this.driveController, driveLayout.driveRot).build();
 
     }
     public static IO getInstance() {
